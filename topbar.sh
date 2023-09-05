07bec42a93bb5c2c7e0b4bcb0c7b558d0326a138
Chandler Barry <whysoseriousscoob@protonmail.com>
2023-09-05 17:43:29 +0000

    update

--- topbar.sh
+++ topbar.sh
@@ -1,8 +1,5 @@
 #!/Bin/Bash
 
-	# Weather Command
-# curl -s wttr.in/"bristol,ct"?format=1
-
 while true; do
 	xsetroot -name "$(free -h | awk '/^Mem:/ {print $3 "/" $2}') | Uptime: $(uptime | awk '{print $3}' | tr -d ',') | $( date )"
 	sleep 1
